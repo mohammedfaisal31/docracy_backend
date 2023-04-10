3b7a51a648bde62e35d4462ca9364caa42dc7411
Mohammed Soharab S <soharab@quinta.co.in>
2023-04-10 17:12:06 +0530

    646423

--- server.js
+++ server.js
@@ -903,7 +903,7 @@ app.get('/receipt/:transaction_id', async (req, res) => {
 })
 
 app.get("/demo",(req,res)=>{
-  res.render('/var/www/html/server/invoice-demo.ejs')
+  res.render('/var/www/html/server/invoice-net.ejs')
 })
 // Starting both http & https servers
 const httpServer = http.createServer(app);
