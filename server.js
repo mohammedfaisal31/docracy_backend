5e73472f4c6e2d1ce60899872b96d36e503ce32e
Mohammed Soharab S <soharab@quinta.co.in>
2023-04-30 23:05:59 +0530

    dfhbe

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.static(__dirname,'public'));
+app.use(express.static('public'));
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 app.set('view engine', 'ejs');
