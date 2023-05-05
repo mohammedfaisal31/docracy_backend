c52e5313f0d22db7ce2ac6dbc19f7b0c97e1a883
Mohammed Soharab S <soharab@quinta.co.in>
2023-05-05 05:52:14 +0530

    djfnrj

--- server.js
+++ server.js
@@ -894,8 +894,8 @@ app.get('/send-invoice/:transaction_id', async (req, res, next) => {
           accompanying_amount_without_gst : formatINR(parseInt(parseInt(details.accompanying_total_amount) * 100/118)),
           accompanying_gst_amount : formatINR((parseInt(details.accompanying_total_amount) * 100/118)*0.18),
           accompanying_total_amount : formatINR(parseInt(details.accompanying_total_amount)),
-          accompanying_total_amount_flag : parseInt(details.accompanying_total_amount)
-          
+          accompanying_total_amount_flag : parseInt(details.accompanying_total_amount),
+          grandTotalAmount : formatINR(parseInt(details.grad_amount))
           
         }
       )
