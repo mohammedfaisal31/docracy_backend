60429701a04975c634898fff4d0f65161cb9c7b6
Mohammed Soharab S <soharab@quinta.co.in>
2023-05-06 13:50:32 +0530

    rkfnr

--- server.js
+++ server.js
@@ -966,15 +966,15 @@ app.get('/send-invoice/:transaction_id', async (req, res, next) => {
                         html: html
                       }
 
-                      // transporter.sendMail(usermailOptions, function(error, info) {
-                      //   if (error) {
-                      //     console.log(error);
-                      //     res.status(500).send('Error sending email');
-                      //   } else {
-                      //     console.log('Email sent: ' + info.response);
-                      //     res.status(200).send('Email sent successfully');
-                      //   }
-                      // })
+                       transporter.sendMail(usermailOptions, function(error, info) {
+                         if (error) {
+                           console.log(error);
+                           res.status(500).send('Error sending email');
+                         } else {
+                           console.log('Email sent: ' + info.response);
+                           res.status(200).send('Email sent successfully');
+                         }
+                       })
                       transporter.sendMail(adminmailOptions, function(error, info) {
                         if (error) {
                           console.log(error);
