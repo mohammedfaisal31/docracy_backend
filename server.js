6bcd26045d8eaf658e36bd88008c1388374c5cf6
unknown <mohammedfaisal3366@gmail.com>
2023-10-11 19:15:46 +0530

    bf

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const credentials = {
 
 // MySQL connection pool configuration
 const pool = mysql.createPool({
-  host: "34.100.199.31",
-  user: "sammy",
+  host: "localhost",
+  user: "root",
   password: "65109@mysql",
   database: "docracy",
   connectionLimit: 10, // Set the maximum number of connections
